89076b07d78ea24c3cc5eb3114916a8a8936eb5b
Humberto Diaz <hdkaratechop@gmail.com>
2022-05-05 01:58:33 -0700

    Add functions to simulate play

--- js-file.js
+++ js-file.js
@@ -6,6 +6,7 @@ function findId (btn)
 }
 
 
+  
 
 
 
@@ -13,6 +14,7 @@ function findId (btn)
 
 
 //generate's a random weapon choice for the computer  
+
 function computerPlay() {
     const weapons = ['rock', 'paper', 'scissors'];
         for (let i = 0; i<2; i++){
@@ -34,74 +36,98 @@ function playerPlay() {
 playerTotal = [];  
 computerTotal = []
 total = []
-const playerScore = document.querySelector('.player-score')
-const computerScore = document.querySelector('.computer-score')
-playerScore.textContent =  individualScore();
-computerScore.textContent = individualScore();
+const playerScore = document.getElementById('player-score')
+const computerScore = document.getElementById('computer-score')
 
-function individualScore() {
-    for (let i = 0; i < total.length; i++) {
-        sum += total[i];
-    }
-    return sum;
+sum = 0;
+
+// iterates through the total variable and returns the sum
+for (let i = 0; i < total.length; i++) {
+sum += total[i];
 }
 
+
+
+playerSum = 0;
+function playerScore() {
+    for (let i = 0; i < total.length; i++) {
+        playerSum += playerTotal[i];
+    }
+    return playerSum;
+}
+computerSum = 0;
+function computerScore() {
+    for (let i = 0; i < total.length; i++) {
+        computerSum += computerTotal[i];
+    }
+    return computerSum;
+}
+
+
+
+function addScore() {
+
+    if (playRound)
+    return  playerScore, computerScore;
+
+}
 
 
 function playRound (playerPlay, computerPlay){
         
     if (playerPlay == computerPlay) {
-        alert("You tied");
-        return  "You tied!";
-    } else if (playerPlay == "paper" && computerPlay == "rock"){
-        return "You Win!" && total.push(1) && playerTotal.push(1);
+        return  document.getElementById('results').innerHTML = 'Hey  you <strong>TIED!</strong>' 
+
+    } else if (playerPlay == "PAPER" && computerPlay == "ROCK"){
+        return document.getElementById('results').innerHTML = 'Hey  you <strong>WIN!</strong>'
+         && total.push(1) && playerTotal.push(1) && playerScore;
         //call the push function to add 1 to the total variable//
-        // call push function again to add 1 to player total not just overall total 
-    } else if (playerPlay == "scissors" && computerPlay == "paper"){
-        return "You Win!" && total.push(1) && playerTotal.push(1);
-    } else if (playerPlay == "rock" && computerPlay == "paper") {
-        return "You Win!" && total.push(1) && playerTotal.push(1);
+        // call push function again to add 1 to player/computer total
+    } else if (playerPlay == "SCISSORS" && computerPlay == "PAPER"){
+        return document.getElementById('results').innerHTML = 'Hey  you <strong>WIN!</strong>'
+         && total.push(1) && playerTotal.push(1) && playerScore;;
+    } else if (playerPlay == "ROCK" && computerPlay == "PAPER") {
+        return document.getElementById('results').innerHTML = 'Hey  you <strong>WIN!</strong>'
+         && total.push(1) && playerTotal.push(1) && playerScore;
     } else {
-        return "You Loose!" && total.push(-1) && computerTotal.push(1); 
+        return document.getElementById('results').innerHTML = 'Try <strong>message 1</strong> again...'
+         && total.push(-1) && computerTotal.push(1) && computerScore;
             //call the push function to subtract 1 to the total variable//
             // call push function again to add 1 to computer total 
 
     }
 }
-// // 
-// function game() {  
+
+
+// 
+function game() {  
     
 
-// //Play game 5 times
-//     for (let i = 0; i < 5; i++) {
-//     // Call playRound function, passing in newly returned values
-//     // from the playerPlay and computerPlay functions and log to console
-//     console.log(playRound(playerPlay(), computerPlay()));
-// }      
-// }
-// game();
+//Play game 5 times
+    for (let i = 0; i < 5; i++) {
+    // Call playRound function, passing in newly returned values
+    // from the playerPlay and computerPlay functions and log to console
+    console.log(playRound(playerPlay(), computerPlay()));
+}      
+}
+game();
 
 
 
 
-// sum = 0;
 
-// // iterates through the total variable and returns the sum
-// for (let i = 0; i < total.length; i++) {
-// sum += total[i];
-// }
 
-// //verifies the summation of the total array 
+//verifies the summation of the total array 
 
-// console.log(sum);
+console.log(sum);
 
-// // after 5 rounds results are calculated based on algorithm below 
+// after 5 rounds results are calculated based on algorithm below 
 
 if (sum >= 1){
-alert ("Congrats! You beat the computer.");
+    document.getElementById('results').innerHTML = 'Hey  you <strong>DEFEATED THE MACHINE</strong>';
 //need to change alert to a div and transform that div to say these words maybe?
 } else if (sum <= -1){
-alert ("You have been defeated by the machine!"); 
+    document.getElementById('results').innerHTML = 'Hey  you <strong>LOST!</strong>'; 
 } else {
-alert ("Not, bad! You tied with the computer.");
+    document.getElementById('results').innerHTML = 'Not, bad! You tied with the computer';
 }
